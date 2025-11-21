2656d4bbaf8d2f21dd6df4fefe375ba26feb0476
eskersaeed <eskersaeed@gmail.com>
2025-11-21 00:13:18 +0000

    ready for deplyment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,37 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 
 const app = express();
+
+// correctly derive __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = process.env.PORT || 3000;
+
+// parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  const distPath = path.join(__dirname, '..', '..', 'frontend', 'dist');
+
+  // serve static assets from the built frontend directory
+  app.use(express.static(distPath));
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
