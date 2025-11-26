cdb2d29d618ca022cbf3343d0856f61eab89e5d6
eskersaeed <eskersaeed@gmail.com>
2025-11-26 00:38:18 +0000

    regex error fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,8 +18,8 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get('*', (_, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  app.get('/.*/', (_, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 } else {
   app.get('/', (req, res) => {
