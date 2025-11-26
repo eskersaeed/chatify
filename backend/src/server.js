f1403b34a43c158dbc237f6fd215c00224583435
eskersaeed <eskersaeed@gmail.com>
2025-11-26 00:53:50 +0000

    error fixes attempt for sevalla deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,29 +3,31 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import path from 'path'; 
+
 dotenv.config();
 
 const app = express();
-const __dirname = path.resolve();
+app.use(express.json()); // parse JSON bodies
 
-const PORT= process.env.PORT || 3000;
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-//make ready for deployment
-
+// make ready for deployment
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get('/.*/', (_, res) => {
+  app.get(/.*/, (_, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 } else {
   app.get('/', (req, res) => {
     res.send('API is running..');
   });
-} 
+}
+
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
 });
